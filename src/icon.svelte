8cc30e29ae441616d9d7381e1ceda9b9b9f36254
Aleksandr Alksandrov <hazg@mail.ru>
2021-01-30 08:58:15 +0300

    init

---
+++ src/icon.svelte
@@ -0,0 +1,37 @@
+
+<script>
+  import Iconify from '@iconify/iconify';
+  import Spinner from './components/spinner';
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    if(!name) return;
+    Iconify.preloadImages([name])
+    document.addEventListener('IconifyAddedIcons', function() {
+      iconData = Iconify.getSVGObject(name)
+      iconExists = Iconify.iconExists(name)
+    });
+  });
+
+  $: iconData = name?Iconify.getSVGObject(name):false
+  $: iconExists = name?Iconify.iconExists(name):false
+  $: iconName = name
+
+  export let name
+</script>
+
+<span class="iconify">
+{#if iconExists}
+  <svg {...iconData.attributes}>
+    {@html iconData.body }
+  </svg>
+{:else}
+  <Spinner />
+{/if}
+</span>
+
+<style>
+  .iconify{
+    text-align: center;
+  }
+</style>
