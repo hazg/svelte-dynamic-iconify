1e1f2b1089b22a9d04c4813814d6e71b30320687
Aleksandr Alksandrov <hazg@mail.ru>
2021-03-12 16:45:56 +0300

    u

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,57 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import sveltePreprocess from 'svelte-preprocess'
+import css from "rollup-plugin-css-only"
+import { terser } from 'rollup-plugin-terser'
+import uglify from "rollup-plugin-uglify-es"
 
-const pkg = require('./package.json');
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
+  perf: true,
   input: 'src/icon.svelte',
+  treeshake: true,
+  external: ['@iconify/iconify'],
+  // preserveModules: true,
   output: [
-    { file: pkg.module, 'format': 'en' },
-    { file: pkg.main, 'format': 'umd', name: 'Icon' }
+    /*{
+      dir: 'dist.es/',
+      exports: 'auto',
+      format: 'es',
+      sourcemap: true,
+      name: 'svelte-dynamic-iconify'
+    },*/
+    {
+      dir: 'dist/',
+      exports: 'auto',
+      format: 'cjs',
+      sourcemap: true,
+      name: 'svelte-dynamic-iconify'
+    }
   ],
   plugins: [
-    svelte(),
-    resolve()
+    svelte({
+      compilerOptions: {
+
+      },
+      emitCss: true,
+      preprocess: sveltePreprocess({
+        hydratable: true,
+        dev: !production,
+       }),
+    }),
+    resolve({
+      extensions: ['.svelte', '.js'],
+			preferBuiltins: false,
+			browser: true,
+			dedupe: importee =>
+				importee === "svelte" || importee.startsWith("svelte/"),
+		}),
+    commonjs(),
+    css(),
+    //{ output: 'bundle.css' }
+    production && uglify(),
+		production && terser(),
   ],
 };
