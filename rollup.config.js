dc6acdfb29292413df5d57d55c111f23d5284998
Aleksandr Alksandrov <hazg@mail.ru>
2021-03-12 14:42:13 +0300

    package

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,13 @@ import resolve from 'rollup-plugin-node-resolve';
 const pkg = require('./package.json');
 
 export default {
-    input: 'src/icon.svelte',
-    output: [
-        { file: pkg.module, 'format': 'en' },
-        { file: pkg.main, 'format': 'umd', name: 'Icon' }
-    ],
-    plugins: [
-        svelte(),
-        resolve()
-    ],
+  input: 'src/icon.svelte',
+  output: [
+    { file: pkg.module, 'format': 'en' },
+    { file: pkg.main, 'format': 'umd', name: 'Icon' }
+  ],
+  plugins: [
+    svelte(),
+    resolve()
+  ],
 };
